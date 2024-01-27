65756cf89fedcd5545eff77633e9e1a12b432e94
AnnaRokh <145573709+AnnaRokh@users.noreply.github.com>
2024-01-27 09:23:29 -0500

    Add names to Class2.java

--- src/package1/Class2.java
+++ src/package1/Class2.java
@@ -6,5 +6,8 @@ public class Class2 {
 
     public static void main(String[] args) {
         System.out.println("Does it work now?");
-    }
+        System.out.println("Jason, Jamshed, Percilla, Seda, Sule, Imane, 
+                           Zeynep, Tatiana, Takhir, Anna, Ciearra, Ahmet, Osman, Anamika");
+ //like this? does it work
+}
 }
